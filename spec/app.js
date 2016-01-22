d8daa9d47fbc67e7146a8a7ea6ecd46e6ada35fb
Amós Rivera <arivera.utpmp@gmail.com>
2016-01-22 11:13:16 -0400

    chore(): add typescript definitions

--- spec/app.js
+++ spec/app.js
@@ -1,6 +1,5 @@
 describe('Check configuration', function () {
     it('is true', function () {
-        var a = 1;
         expect(true).toBe(true);
     });
 });
--- spec/app.ts
+++ spec/app.ts
@@ -1,6 +1,5 @@
 describe('Check configuration', () => {
   it ('is true', () => {
-    var a:string = 1;
     expect(true).toBe(true);
   })
 });
